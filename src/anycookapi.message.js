8107ab6bebb725ed99368b06808e65004c51905c
Jan Graßegger <krankgesund@googlemail.com>
2014-03-12 13:36:44 +0100

    updated message function

--- src/anycookapi.message.js
+++ src/anycookapi.message.js
@@ -22,108 +22,108 @@
 'use strict';
 //message([lastdate [, callback]])
 AnycookAPI.message = function(){
-	var lastdate;
-	var callback;
-	switch(arguments.length){
-	case 2:
-		var type2 = typeof arguments[1];
-		if(type2 === 'function'){
-			callback = arguments[1];
-		}
-		/* falls through */
-	case 1:
-		var type1 = typeof arguments[0];
-		if(type1 === 'string' || type1 === 'number'){
-			lastdate = Number(arguments[0]);
-		}
-		else if(type1 === 'function'){
-			callback = arguments[0];
-		}
-	}
+    var lastdate;
+    var callback;
+    switch(arguments.length){
+    case 2:
+        var type2 = typeof arguments[1];
+        if(type2 === 'function'){
+            callback = arguments[1];
+        }
+        /* falls through */
+    case 1:
+        var type1 = typeof arguments[0];
+        if(type1 === 'string' || type1 === 'number'){
+            lastdate = arguments[0];
+        }
+        else if(type1 === 'function'){
+            callback = arguments[0];
+        }
+    }
 
-	var path = '/message';
-	var data = {lastChange:lastdate};
-	return AnycookAPI._get(path, data, callback);
+    var path = '/message';
+    var data = {lastChange:lastdate};
+    return AnycookAPI._get(path, data, callback);
 };
 
 $.extend(AnycookAPI.message, {
-	//session(sessionid [,lastid] [,callback])
-	session : function(sessionid){
-		var callback;
-		var lastid = -1;
+    //session(sessionid [,lastid] [,callback])
+    session : function(sessionid){
+        var callback;
+        var lastid = -1;
 
-		switch(arguments.length){
-		case 3:
-			var type2 = typeof arguments[2];
-			if(type2 === 'function'){
-				callback = arguments[2];
-			}
-			/* falls through */
-		case 2:
-			var type1 = typeof arguments[1];
-			if(type1 === 'number' || type1 === 'string'){
-				lastid = Number(arguments[1]);
-			}
-			else if(type1 === 'function'){
-				callback = arguments[1];
-			}
-		}
+        switch(arguments.length){
+        case 3:
+            var type2 = typeof arguments[2];
+            if(type2 === 'function'){
+                callback = arguments[2];
+            }
+            /* falls through */
+        case 2:
+            var type1 = typeof arguments[1];
+            if(type1 === 'number' || type1 === 'string'){
+                lastid = Number(arguments[1]);
+            }
+            else if(type1 === 'function'){
+                callback = arguments[1];
+            }
+        }
 
-		var path = '/message/'+sessionid;
-		var data = {lastId : lastid};
-		return AnycookAPI._get(path, data, callback);
-	},
-	//number([lastnum] [,callback, error])
-	number : function(){
-		var lastnum = -1;
-		var callback;
+        var path = '/message/'+sessionid;
+        var data = {lastId : lastid};
+        return AnycookAPI._get(path, data, callback);
+    },
+    //number([lastnum] [,callback, error])
+    number : function(){
+        var lastnum = -1;
+        var callback;
         var error;
 
-		switch(arguments.length){
+        switch(arguments.length){
         case 3:
             var type3 = typeof arguments[1];
             if(type3 === 'function'){
                 error = arguments[2];
             }
             /* falls through */
-		case 2:
-			var type2 = typeof arguments[1];
-			if(type2 === 'function'){
-				callback = arguments[1];
-			}
-			/* falls through */
-		case 1:
-			var type1 = typeof arguments[0];
-			if(type1 === 'number' || type1 === 'string'){
-				lastnum = Number(arguments[0]);
-			}
-			else if(type1 === 'function'){
-				callback = arguments[1];
-			}
-		}
+        case 2:
+            var type2 = typeof arguments[1];
+            if(type2 === 'function'){
+                callback = arguments[1];
+            }
+            /* falls through */
+        case 1:
+            var type1 = typeof arguments[0];
+            if(type1 === 'number' || type1 === 'string'){
+                lastnum = Number(arguments[0]);
+            }
+            else if(type1 === 'function'){
+                callback = arguments[1];
+            }
+        }
 
-		var path = '/message/number';
-		var data = {lastNum : lastnum};
+        var path = '/message/number';
+        var data = {lastNum : lastnum};
 
-		var settings = AnycookAPI._settings();
-		$.extend(data, {appid : settings.appid});
+        var settings = AnycookAPI._settings();
+        $.extend(data, {appid : settings.appid});
 
-		return AnycookAPI._get(path, data, callback);
-	},
-	//writeMessage(sessionid, text, [,callback])
-	answer : function(sessionid, text, callback){
-		var path = '/message/'+sessionid;
-		AnycookAPI._postJSON(path, text, callback);
-	},
-	//writeNewMessage(recipients, text [, callback])
-	writeNew : function(recipients, text, callback){
-		var path = '/message';
-		var data = {text:text, recipients:recipients};
-		AnycookAPI._postJSON(path, data, callback);
-	},
-	//readMessage(sessionid, messageid [,callback])
-	read : function(sessionid, messageid, callback){
-		var path = '/message/'+sessionid+'/'+messageid;
-		AnycookAPI._put(path, {}, callback);
-	}
+        return AnycookAPI._get(path, data, callback);
+    },
+    //writeMessage(sessionid, text, [,callback])
+    answer : function(sessionid, text, callback){
+        var path = '/message/'+sessionid;
+        AnycookAPI._postJSON(path, text, callback);
+    },
+    //writeNewMessage(recipients, text [, callback])
+    writeNew : function(recipients, text, callback){
+        var path = '/message';
+        var data = {text:text, recipients:recipients};
+        AnycookAPI._postJSON(path, data, callback);
+    },
+    //readMessage(sessionid, messageid [,callback])
+    read : function(sessionid, messageid, callback){
+        var path = '/message/'+sessionid+'/'+messageid;
+        AnycookAPI._put(path, {}, callback);
+    }
 });
